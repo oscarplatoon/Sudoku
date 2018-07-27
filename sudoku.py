6754fd259bb58818418256e141f63af20af0ce69
Joshua Alletto <joshalletto@gmail.com>
2018-07-27 14:26:35 -0500

    edit read me.

--- sudoku.py
+++ sudoku.py
@@ -5,9 +5,7 @@ class SudokuSolver:
   def solve(self):
     
 
-  # Don't worry about this to start with. It's here as a place holder for when
-  # you're ready to make your board visually appealing. Focus on implementing the
-  # algo first
+
   def board(self):
     
   
@@ -15,8 +13,8 @@ class SudokuSolver:
 
 # The file has newlines at the end of each line, so we call
 # String#chomp to remove them.
-game = SudokuSolver(board_string)
-# Remember: this will just fill out what it can and not "guess"
-game.solve
+# game = SudokuSolver(board_string)
+# # Remember: this will just fill out what it can and not "guess"
+# game.solve
 
-print(game.board)
+# print(game.board)
