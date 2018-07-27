ba1ccb45410370c5c5caa0a38b16ce44a5c1a5c7
Joshua Alletto <joshalletto@gmail.com>
2018-07-27 14:12:39 -0500

    r to p

--- sudoku.py
+++ sudoku.py
@@ -1,24 +1,24 @@
-class Rudoku
-  def initialize(board_string)
-  end
+class SudokuSolver:
+  def __init__(board_string):
 
-  def solve!
-  end
+
+  def solve():
+  
 
   # Don't worry about this to start with. It's here as a place holder for when
   # you're ready to make your board visually appealing. Focus on implementing the
   # algo first
-  def board
+  def board():
 
-  end
-end
+  
+
 
 # The file has newlines at the end of each line, so we call
 # String#chomp to remove them.
 
-game = Rudoku.new(board_string)
+game = SudokuSolver(board_string)
 
 # Remember: this will just fill out what it can and not "guess"
-game.solve!
+game.solve
 
-puts game.board
+print(game.board)
