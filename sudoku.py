1429b68e8f38a8583efe7049c9358b25bb901918
Won Yang <won@gatech.edu>
2021-06-06 19:35:40 -0500

    not working solution

--- sudoku.py
+++ sudoku.py
@@ -5,7 +5,8 @@ import random
 
 class SudokuSolver:
     def __init__(self):
-        self.board = []  # contains Cell objects
+        # contains 9 row arrays that contain numbers
+        self.board = [[], [], [], [], [], [], [], [], []]
         initial_board_states = []
 
         # fills out initial_board_states as strings of nums
@@ -19,39 +20,73 @@ class SudokuSolver:
         # gets a random board and assigns it
         rand_board_index = random.randrange(len(initial_board_states))
         initial_board_str = initial_board_states[rand_board_index]
-        for char in initial_board_str:
-            self.board.append(Cell(int(char)))
+
+        # adds nums to self.board
+        row_index = 0
+        for i, char in enumerate(initial_board_str):
+            self.board[row_index].append(int(char))
+            if i % 9 == 8:
+                row_index += 1
+
+    def find_next_zero(self):
+        for row_index in range(9):
+            for col_index in range(9):
+                if self.board[row_index][col_index] == 0:
+                    return row_index, col_index
+
+    def is_valid(self, guess, row_index, col_index):
+        # check row
+        row_vals = self.board[row_index]
+        if guess in row_vals:
+            return False
+
+        # check column
+        for i in range(9):
+            if self.board[i][col_index] == guess:
+                return False
+
+        # check 3x3 box
+        row_index_start = row_index // 3
+        col_index_start = col_index // 3
+        for row_index_2 in range(row_index_start, row_index_start + 3):
+            for col_index_2 in range(col_index_start, col_index_start + 3):
+                if self.board[row_index_2][col_index_2] == guess:
+                    return False
+
+        # if is valid
+        return True
 
     def solve(self):
-        pass
+        row_index, col_index = self.find_next_zero()
+
+        # if sudoku is solved
+        if row_index is None:
+            return
+
+        for guess in range(1, 10):
+            if self.is_valid(guess, row_index, col_index):
+                self.board[row_index][col_index] = guess
+                self.solve()
+            else:
+                continue
 
     def print_board(self):
         print("---------------------")
-        for i, cell in enumerate(self.board):
-            print(cell.num, end=" ")
-            if i % 9 == 8:
-                print()
-            elif i % 3 == 2:
-                print("", end="| ")
-            if i % 27 == 26:
+        for i, row in enumerate(self.board):
+            for j, num in enumerate(row):
+                print(num, end=" ")
+                if j % 9 == 8:
+                    pass
+                elif j % 3 == 2:
+                    print("| ", end="")
+            print()
+            if i % 3 == 2:
                 print("---------------------")
 
 
-class Cell:
-    def __init__(self, num) -> None:
-        self.num = num
-
-    def get_other_row_cells(self):
-        pass
-
-    def get_other_column_cells(self):
-        pass
-
-    def get_other_box_cells(self):
-        pass
-
-
-game = SudokuSolver()
-game.print_board()
-# game.solve
-# game.print_board()
+if __name__ == "__main__":
+    game = SudokuSolver()
+    game.print_board()
+    game.solve()
+    print()
+    game.print_board()
